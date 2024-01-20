a73ceea8aa81dbc7306c9c0b80dcb233e816079d
Jhon-clase1 <jhoncensa2023@gmail.com>
2024-01-20 11:49:08 -0500

    ejercicio 2 corregido

--- ejercicio 2/contraseñas.py
+++ ejercicio 2/contraseñas.py
@@ -1,9 +1,9 @@
-import rando
+import random # se corrigio el nombre rando a random, para poder importar bien la funcion
 import string
 
-generar_contraseña(longitud=8):
+def generar_contraseña(longitud=8): # se corrigio la sintaxis de la funcion, ya que le falta la palabra clave "def" antes del nombre de la función
     caracteres = string.ascii_letters + string.digits + string.punctuation
     contraseña = ''.join(random.choice(caracteres) for _ in range(longitud))
-    retunr contraseña
+    return contraseña # se corrigio la palabra return
 
-print(generar_contrasena())
+print(generar_contraseña()) # se corrigio la funcion, por que se estaba escribiendo con n, y esta escrita con ñ. asi que por eso aparecia indefinida, o desconocida
