42dbd1928145e883a6cba0e93a3c818ef0c4b8b1
JhonGar12358 <jhoncensa2023@gmail.com>
2024-01-27 08:51:15 -0500

    ejercicio 6 corregido

--- ejercicio 6/palindromo.py
+++ ejercicio 6/palindromo.py
@@ -1,9 +1,9 @@
-def es_palindromo(texto): # Se le agragaron los dos puntos al final, para poder tener la sintaxis correcta de la función
+def es_palindromo(texto): # Se le agragaron los dos puntos al final, para poder tener bien la sintaxis correcta de la función
     texto = ''.join(caracter.lower() for caracter in texto if caracter.isalnum())
     return texto == texto[::-1]
 
 palabra_frase = input("Ingrese una palabra o frase: ")
-if es_palindromo():
+if es_palindromo(palabra_frase): # se añadio el argumento necesario para que la funcion valide si la palabra ingresada es palindroma.
     print(f"{palabra_frase} es un palíndromo.")
 else:
     print(f"{palabra_frase} no es un palíndromo.")
